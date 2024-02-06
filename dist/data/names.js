085db767cd978c727133075b210c9dc7786f8487
davidinwald <david.inwald@gmail.com>
2024-02-06 13:28:44 -0500

    copy data dir into dist

---
+++ dist/data/names.js
@@ -0,0 +1,103 @@
+export const firstNames = [
+  'John',
+  'Jane',
+  'Jack',
+  'Jill',
+  'James',
+  'Jenny',
+  'Joe',
+  'Jade',
+  'Jasper',
+  'Jade',
+  'Jared',
+  'Jasmine',
+  'Jared',
+  'Mohammed',
+  'Sophia',
+  'Liam',
+  'Olivia',
+  'Noah',
+  'Emma',
+  'Oliver',
+  'Ava',
+  'Elijah',
+  'Isabella',
+  'Yusuf',
+  'Emily',
+  'Jacob',
+  'Sophie',
+  'Michael',
+  'Grace',
+  'Daniel',
+  'Chloe',
+  'Matthew',
+  'Lily',
+  'Mia',
+  'Lucas',
+  'Amelia',
+  'Alexander',
+  'Evie',
+  'Jacob',
+  'Ella',
+  'Sruthi',
+];
+
+export const lastNames = [
+  'Smith',
+  'Jones',
+  'Taylor',
+  'Brown',
+  'Williams',
+  'Wilson',
+  'Johnson',
+  'Davies',
+  'Robinson',
+  'Wright',
+  'Thompson',
+  'Evans',
+  'Walker',
+  'White',
+  'Roberts',
+  'Green',
+  'Hall',
+  'Wood',
+  'Jackson',
+  'Clarke',
+  'Patel',
+  'Khan',
+  'Lewis',
+  'James',
+  'Phillips',
+  'Mason',
+  'Mitchell',
+  'Rose',
+  'Davis',
+  'Rodriguez',
+  'Cox',
+  'Alexander',
+  'Garden',
+  'Campbell',
+  'Johnston',
+  'Moore',
+  'Smyth',
+  "O'Neill",
+  'Anderson',
+  'Scott',
+];
+
+const generateName = () => {
+  const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+  const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+  return `${firstName} ${lastName}`;
+};
+
+const generateNames = (numNames = 1) => {
+  const names = [];
+  for (let i = 0; i < numNames; i++) {
+    names.push(generateName());
+  }
+  return names;
+};
+
+module.exports = generateNames;
---
+++ dist/data/names.spec.js
@@ -0,0 +1,17 @@
+// BEGIN: abpxx6d04wxr
+const { test, expect } = require('@playwright/test');
+const generateNames = require('./names');
+
+test('Verify the name generator function returns a valid name', async () => {
+  // Generate sample data
+  const testNames = generateNames(1);
+  const testName = testNames[0];
+
+  // Perform assertions to verify the generated data
+  // should a name of format "firstName lastName"
+  expect(testName).toMatch(/^[A-Z][a-z]+ [A-Z][a-z]+$/);
+  // should not contain any numbers
+  expect(testName).not.toMatch(/\d/);
+  // should not contain any special characters
+  expect(testName).not.toMatch(/[^a-zA-Z ]/);
+});
