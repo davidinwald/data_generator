82349b8a623946d9a31ba9c7e204eb702d9ae47d
davidinwald <david.inwald@gmail.com>
2024-02-06 07:45:43 -0500

    moved ts into /source, built js into /dist, fixed linter

---
+++ dist/dataGenerator.js
@@ -0,0 +1,82 @@
+'use strict';
+const generateNames = require('./data/names');
+// this function generates sample data that can be helpful for dev purposes
+// input parameters include the number of columns, number of rows,
+// and the type of data for each column
+const generateData = ({ numRows, colTypes }) => {
+  const data = [];
+  const columnArrays = colTypes.map((colType) => {
+    return {
+      column: colType.name,
+      values: generateDataByType(
+        Object.assign(Object.assign({}, colType), { numRows }),
+      ),
+    };
+  });
+  for (let i = 0; i < numRows; i++) {
+    const row = {};
+    for (let j = 0; j < colTypes.length; j++) {
+      const colName = colTypes[j].name;
+      if (!row[colName]) {
+        row[colName] = columnArrays[j].values[i];
+      }
+    }
+    data.push(row);
+  }
+  return data;
+};
+module.exports = generateData;
+// Path: dataGenerator.js
+// this function generates a single value of a given type
+const generateDataByType = ({ type, min, max, subType, numRows }) => {
+  switch (type) {
+    case 'string':
+      return generateString({ subType: subType || '', numRows });
+    case 'integer':
+      return generateInteger({ min, max, numRows });
+    case 'number':
+      return generateNumber({ min, max, numRows });
+    case 'boolean':
+      return generateBoolean({ truthRatio: 0.9, numRows });
+    // case 'date':
+    //   return generateDate({ numRows });
+    default:
+      return generateString({ subType: subType || '', numRows });
+  }
+};
+// this function generates a random string
+const generateString = ({ subType, numRows }) => {
+  if (subType === 'name') {
+    return generateNames(numRows);
+  }
+  const length = Math.floor(Math.random() * 10) + 1;
+  let str = '';
+  for (let i = 0; i < length; i++) {
+    const charCode = Math.floor(Math.random() * 26) + 97;
+    str += String.fromCharCode(charCode);
+  }
+  return str.repeat(numRows);
+};
+// this function generates a random boolean
+const generateBoolean = ({ truthRatio, numRows }) => {
+  return Array(numRows)
+    .fill(undefined, 0, numRows)
+    .map(() => Math.random() < truthRatio);
+};
+// this function generates a random integer within a specified range
+const generateInteger = ({ min = 0, max = 100, numRows }) => {
+  return Array(numRows).fill(Math.floor(Math.random() * (max - min + 1)) + min);
+};
+// this function generates a random number within a specified range
+const generateNumber = ({ min = 0, max = 100, precision = 5, numRows }) => {
+  const range = max - min;
+  const randomValues = Array(numRows)
+    .fill(undefined, 0, numRows)
+    .map(() => {
+      const randomValue = Math.random() * range + min;
+      const multiplier = Math.pow(10, precision);
+      const roundedValue = Math.round(randomValue * multiplier) / multiplier;
+      return roundedValue;
+    });
+  return randomValues;
+};
---
+++ dist/seriesGenerator.js
@@ -0,0 +1,46 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.generateLogSeries =
+  exports.generateQuadraticSeries =
+  exports.generateLinearSeries =
+    void 0;
+function generateLinearSeries({
+  start,
+  end,
+  step,
+  standardDeviation = 0,
+  precision = 3,
+  yIntercept = 0,
+  slope = 1,
+}) {
+  const series = [];
+  for (let i = start; i <= end; i += step) {
+    const noise = standardDeviation * Math.random();
+    const value = yIntercept + slope * i + noise;
+    const roundedValue =
+      precision && Number.isInteger(precision) && precision > 0
+        ? Number(value.toFixed(precision))
+        : value;
+    series.push(roundedValue);
+  }
+  return series;
+}
+exports.generateLinearSeries = generateLinearSeries;
+function generateQuadraticSeries({ start, end, step, standardDeviation = 0 }) {
+  const series = [];
+  for (let i = start; i <= end; i += step) {
+    const noise = standardDeviation * Math.random();
+    series.push(i * i + noise);
+  }
+  return series;
+}
+exports.generateQuadraticSeries = generateQuadraticSeries;
+function generateLogSeries({ start, end, step, standardDeviation = 0 }) {
+  const series = [];
+  for (let i = start; i <= end; i += step) {
+    const noise = standardDeviation * Math.random();
+    series.push(Math.log(i) + noise);
+  }
+  return series;
+}
+exports.generateLogSeries = generateLogSeries;
